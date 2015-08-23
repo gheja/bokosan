19830aaf04cea4a997ac00574a15f631e1fbcb73
Gabor Heja <gheja@m002560p>
2015-08-23 14:28:02 +0200

    build.sh: made some optimizations

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+name="bokosan"
+
 try()
 {
 	$@
@@ -13,7 +15,7 @@ try()
 
 [ -d build ] || try mkdir -p -m 700 build
 [ -d build/compiler ] || try mkdir -p -m 700 build/compiler
-[ -d build/bokosan ] || try mkdir -p -m 700 build/bokosan
+[ -d build/game ] || try mkdir -p -m 700 build/game
 
 which java 2>/dev/null >/dev/null
 if [ $? != 0 ]; then
@@ -45,43 +47,34 @@ fi
 echo "* Closure Compiler seems to be good."
 
 echo "* Cleaning up build directories..."
-rm build/bokosan/* || /bin/true
+rm build/game/* || /bin/true
 
 echo "* Copying files..."
-cp -v ./src/jsfxr.js ./src/synth.js ./src/main.js ./src/package.json ./src/tileset.png ./build/bokosan/
+cp -v ./src/index.html ./src/jsfxr.js ./src/synth.js ./src/main.js ./src/package.json ./src/tileset.png ./build/game/
 
 echo "* Removing debug sections, merging files and renaming some variables..."
-cat ./build/bokosan/jsfxr.js ./build/bokosan/synth.js ./build/bokosan/main.js | sed \
-	-e '/DEBUG BEGIN/,/\DEBUG END/{d}' > ./build/bokosan/merged.js
+cat ./build/game/jsfxr.js ./build/game/synth.js ./build/game/main.js | sed \
+	-e '/DEBUG BEGIN/,/\DEBUG END/{d}' > ./build/game/merged.js
 
 echo "* Running Closure Compiler..."
 try java -jar ./build/compiler/compiler.jar \
 	--compilation_level ADVANCED_OPTIMIZATIONS \
 	--use_types_for_optimization \
 	--externs ./src/externs.js \
-	--js ./build/bokosan/merged.js \
-	--js_output_file ./build/bokosan/merged.min.js
+	--js ./build/game/merged.js \
+	--js_output_file ./build/game/merged.min.js
 
 echo "* Embedding js into index.html..."
-{
-	cat ./src/index.html | grep -vE '<script ' | while read line; do
-		echo "$line" | grep -Eq '<!-- insert minified javascript here -->'
-		if [ $? == 0 ]; then
-			echo "<script>"
-			cat ./build/bokosan/merged.min.js
-			echo "</script>"
-		else
-			echo "$line"
-		fi
-	done
-} > ./build/bokosan/index.html
+cat ./build/game/index.html | sed -E 's,<script[^>]+></script>,,gi' | sed -e '/<!-- insert minified javascript here -->/{r ./build/game/merged.min.js' -e 'd}' > ./build/game/index.html.tmp
+mv ./build/game/index.html.tmp ./build/game/index.html
 
 echo "* Optimizing index.html..."
-cat ./build/bokosan/index.html | tr '\n' ' ' | sed -r 's/\s+/ /g' | sed -e 's/> </></g' > ./build/bokosan/index.html.tmp
-try mv -v ./build/bokosan/index.html.tmp ./build/bokosan/index.html
+cat ./build/game/index.html | sed -r 's/^\s+//g' | tr -d '\r' | tr '\n' ' ' | sed -e 's/> </></g' > ./build/game/index.html.tmp
+# cat ./build/game/index.html | tr -d '\r' | tr '\n' ' ' | sed -r 's/\s+/ /g' | sed -e 's/> </></g' > ./build/game/index.html.tmp
+mv ./build/game/index.html.tmp ./build/game/index.html
 
 echo "* Cleaning up..."
-try rm -v ./build/bokosan/*.js
+try rm -v ./build/game/*.js
 
 try cd ./build
 
@@ -89,11 +82,16 @@ now=`date +%Y%m%d_%H%M%S`
 git_id=`git log -1 --format="%H" 2>/dev/null`
 if [ "$git_id" == "" ]; then
 	git_id="unknown"
+else
+	git status --porcelain | grep -vE '^\?\?' | wc -l | grep -Eq '^0$'
+	if [ $? != 0 ]; then
+		git_id="${git_id}-modified"
+	fi
 fi
-zip_file="bokosan_${now}_${git_id}.zip"
+zip_file="${name}_${now}_${git_id}.zip"
 
 echo "* Creating new archive ${zip_file} ..."
-try zip ${zip_file} -r -9 ./bokosan
+try zip ${zip_file} -r -9 ./game
 
 try cd ..
 
@@ -101,7 +99,7 @@ echo "Done."
 
 echo ""
 
-du -b ./src ./build/bokosan ./build/${zip_file}
+du -b ./src ./build/game ./build/${zip_file}
 
 echo ""
 
