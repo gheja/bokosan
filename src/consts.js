df92e557e4b4ef218d4b4e254aa31f652eb3b120
Gabor Heja <gheja@m002560p>
2015-09-01 22:44:03 +0200

    Game: added menu structure

--- src/consts.js
+++ src/consts.js
@@ -17,6 +17,9 @@
 /** @const @type {number} */ var SCREEN_HOWTO = 7;
 
 /** @const @type {number} */ var MENU_MAIN = 0;
+/** @const @type {number} */ var MENU_PLAY = 1;
+/** @const @type {number} */ var MENU_OPTIONS = 2;
+/** @const @type {number} */ var MENU_PAUSED = 3;
 
 /** @const @type {number} */ var ACTION_OPEN_MENU = 0;
 /** @const @type {number} */ var ACTION_CHANGE_SCREEN = 1;
