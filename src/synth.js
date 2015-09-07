4d2fc983134f1c36806ac577036416ab30826f15
Gabor Heja <gheja@m002560p>
2015-09-07 13:47:51 +0200

    Added synth.js that was missing from commit 8139f5e7c587e834b27e37cffcf540f680941140

---
+++ src/synth.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * @constructor
+ */
+var Synth = function()
+{
+	this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+	this.samples = [];
+	this.patterns = [];
+}
+
+Synth.prototype.addSamples = function(a)
+{
+	var i, sample;
+	
+	for (i in a)
+	{
+		this.samples.push(Jsfxr(this.ctx, a[i]));
+	}
+}
+
+Synth.prototype.playSound = function(id)
+{
+	var source = this.ctx.createBufferSource();
+	source.connect(this.ctx.destination);
+	source.buffer = this.samples[id];
+	source.playbackRate.value = 1;
+	// source.noteOn(0);
+	source.start();
+}
