7ac841b859c3348ada68711085149c4ff1cf4695
Gabor Heja <gheja@m002560p>
2015-09-08 04:13:01 +0200

    ScreenLevels: fixed "A+" display issue

--- src/screenlevels.js
+++ src/screenlevels.js
@@ -111,7 +111,7 @@ ScreenLevels.prototype.drawSelectionOptions = function()
 		this.drawPreview(game, j, p1, p2);
 		
 		a = game.getScores(j - 1);
-		if (a[1][i] <= game.levels[j - 1][LEVEL_DATA_APLUS])
+		if (a[1][0] <= game.levels[j - 1][LEVEL_DATA_APLUS])
 		{
 			game.ctx.drawImage(game._asset, 83, 38, 12, 9, p1 + 80, p2 - 4, 12, 9);
 		}
