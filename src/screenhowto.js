f02b72d14783a39c55247fbfb303859d0852ba76
Gabor Heja <gheja@m002560p>
2015-09-08 04:36:25 +0200

    ScreenHowto: added content

--- src/screenhowto.js
+++ src/screenhowto.js
@@ -21,5 +21,26 @@ ScreenHowto.prototype.tick = function(game)
 
 ScreenHowto.prototype.draw = function(game)
 {
-	game.drawSmallText(0, 270, "(HOWTO SCREEN)");
+	game.drawHeader();
+	game.drawBigText(0, 50, "KEYBOARD");
+	game.drawSmallText(24, 70,
+		" u  W  MOVE UP\n" +
+		" d  S  MOVE DOWN\n" +
+		" <  A  MOVE LEFT\n" +
+		" >  D  MOVE RIGHT\n" +
+		"SHIFT  ACCEPT/NEXT/GRAB\n" +
+		"  ESC  CANCEL/BACK"
+	);
+	game.drawBigText(0, 150, "TOUCH DEVICE");
+	game.drawSmallText(24, 170,
+		"SWIPE ON THE RIGHT HALF OF THE SCREEN:\n" +
+		"  MOVE IN SWIPE DIRECTION\n" +
+		"\n" +
+		"TOUCH BOTTOM LEFT CORNER:\n" +
+		"  ACCEPT/NEXT/GRAB\n" +
+		"\n" +
+		"TOUCH TOP LEFT CORNER:\n" +
+		"  CANCEL/BACK"
+	);
+	game.drawTouchToContinue(96, 270);
 }
