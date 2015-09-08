6e8860305d40ec56fc9aebade4449bc18d056ff9
Gabor Heja <gheja@m002560p>
2015-09-08 04:43:14 +0200

    ScreenIntro: added some half-dummy content

--- src/screenintro.js
+++ src/screenintro.js
@@ -6,20 +6,51 @@
  */
 var ScreenIntro = function()
 {
+	this.tickCount = 0;
+	this.scrollerPosition = 0;
 }
 
 ScreenIntro.prototype = new Screen2();
 
 ScreenIntro.prototype.init = function(game)
 {
+	this.scrollerPosition = 0;
 	game.setWaitForKeypress(SCREEN_MENU);
 }
 
 ScreenIntro.prototype.tick = function(game)
 {
+	this.tickCount++;
+	this.scrollerPosition++;
 }
 
 ScreenIntro.prototype.draw = function(game)
 {
-	game.drawSmallText(0, 270, "(INTRO SCREEN)");
+	var s;
+	
+	//   1234567890123456789012345678901234567890123456789012
+	if (this.scrollerPosition < 50)
+	{
+		s = "MONDAY, 07:50\n"+
+			"\n" +
+			"IN 10 MINUTES YOUR FIST DAY WILL BEGIN OF YOUR NEW\n" +
+			"JOB AS A WAREHOUSE KEEPER.\n";
+			
+		game.drawSmallText(0, 150, s.substr(0, this.scrollerPosition * 3));
+	
+	}
+	else // if (this.scrollerPosition < 60)
+	{
+		s = "MR BOSS:\n" +
+			"  LOREM IPSUM DOLOR SIT AMET PEOPLE WE HIRED IN THE\n" + 
+			"  LAST 30 YEARS TO PUT EVERYTHING IN PLACE BUT ASDFG\n" +
+			"  THAT WAS A HUGE MISTAKE AND...\n";
+		
+		game.drawSmallText(0, 150, s.substr(0, (this.scrollerPosition - 50) * 3));
+	}
+	
+	if (this.scrollerPosition > 110)
+	{
+		game.drawTouchToContinue(96, 270);
+	}
 }
