1a65ed6088854c367cb10b353848bed7b22ba4f2
Gabor Heja <gheja@m002560p>
2015-09-10 21:49:41 +0200

    ScreeChallenges: added.

--- src/screenchallenges.js
+++ src/screenchallenges.js
@@ -2,24 +2,53 @@
 
 /**
  * @constructor
- * @extends {Screen}
+ * @extends {ScreenLevels}
  */
 var ScreenChallenges = function()
 {
 }
 
-ScreenChallenges.prototype = new Screen2();
+ScreenChallenges.prototype = new ScreenLevels();
 
 ScreenChallenges.prototype.init = function(game)
 {
-	game.setWaitForKeypress(SCREEN_MENU);
+	this.offline = 1;
+	this.selection = 1;
+	this.min = 1;
+	this.max = 1;
+	this.unlockedCount = 1;
 }
 
-ScreenChallenges.prototype.tick = function(game)
+ScreenChallenges.prototype.go = function(game)
 {
+	game.gameMode = GAME_MODE_LOCAL;
+	game.nextLevelIndex = this.selection - 1;
+	game.screenFadeAndSwitch(SCREEN_GAME);
 }
 
-ScreenChallenges.prototype.draw = function(game)
+ScreenChallenges.prototype.drawSelectionBox = function()
 {
-	game.drawSmallText(0, 270, "(CHALLENGES SCREEN)");
+	game.ctx.fillStyle = '#000';
+	game.ctx.fillRect((((this.selection - 1) % 6) % 2) * 110 + 16, Math.floor(((this.selection - 1) % 6) / 2) * 80 + 46, 96, 72);
+}
+
+ScreenChallenges.prototype.drawPreview = function(game, j, p1, p2)
+{
+	var x, y, l, padX, padY, a, b, color;
+	
+	game.drawSmallText(p1 + 4, p2 + 4, "LEVEL 1-09\nLEVEL 1-10\nLEVEL 1-11\nLEVEL 1-12");
+}
+
+ScreenChallenges.prototype.drawStats = function()
+{
+	var a, s, i;
+	
+	game.drawBigText(232, 35, "CHALLENGE " + game.pad(this.selection, 2, '0'));
+}
+
+ScreenChallenges.prototype.init = function(game)
+{
+	// this.max = game.levels.length;
+	this.max = 6;
+	this.unlockedCount = 2;
 }
