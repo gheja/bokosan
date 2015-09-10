4ca1eaaa109282243d47188ce5792ccc67c8639a
Gabor Heja <gheja@m002560p>
2015-09-10 03:02:47 +0200

    externs.js: added io stuffs

--- src/externs.js
+++ src/externs.js
@@ -2,3 +2,11 @@ var imageSmoothingEnabled = false;
 var mozImageSmoothingEnabled = false;
 var webkitImageSmoothingEnabled = false;
 var msImageSmoothingEnabled = false;
+
+var io = {};
+io.connect = function(a){};
+io.connect.on = function(a, b){};
+io.connect.emit = function(a, b){};
+io.listen = function(a){};
+io.sockets = {};
+io.sockets.socket = {};
