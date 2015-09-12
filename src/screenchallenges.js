ba8a470e710fe03a7801d4c4a1719d28865069cb
Gabor Heja <gheja@m002560p>
2015-09-12 23:01:01 +0200

    ScreenChallenges: the trophies are now shown in the top list

--- src/screenchallenges.js
+++ src/screenchallenges.js
@@ -76,7 +76,7 @@ ScreenChallenges.prototype.drawPreview = function(game, j, p1, p2)
 
 ScreenChallenges.prototype.drawStats = function()
 {
-	var a, s, i, j;
+	var a, s, i, j, place;
 	
 	// game.drawBigText(232, 35, "CHALLENGE " + game.pad(this.selection, 2, '0'));
 	game.drawBigText(232, 35, "CHALLENGE " + this.selection);
@@ -96,10 +96,34 @@ ScreenChallenges.prototype.drawStats = function()
 	j = 0;
 	s = "";
 	a = game.challengeScores[this.selection - 1];
+	place = 1;
 	for (i=0; i<a.length; i++)
 	{
 		// [challenge id] => [ challenge id, moves, player uid, player name, colors[3][3] ]
-		s += "a  " + a[i][3] + " " + a[i][1] + "\n";
+		
+		if (i > 0 && a[i][1] != a[i - 1][1])
+		{
+			place++;
+		}
+		
+		if (place == 1)
+		{
+			s += "a";
+		}
+		else if (place == 2)
+		{
+			s += "b";
+		}
+		else if (place == 3)
+		{
+			s += "c";
+		}
+		else
+		{
+			s += " ";
+		}
+		
+		s += "  " + game.rpad(a[i][3], 10, ' ') + game.pad(game.thousandPad(a[i][1]), 9, ' ') + "\n";
 		
 		game.drawImageAdvanced(game._asset, game.ctx, 107, 38, 10, 9, 244, 65 + i * 10, 10, 9, 0, 0, a[i][4]);
 	}
