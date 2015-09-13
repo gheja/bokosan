f4d538a871925a601e9496a1b5a8b270c68f9e04
Gabor Heja <gheja@m002560p>
2015-09-13 20:47:37 +0200

    PlayerObj: fixed missing default value for player name

--- src/playerobj.js
+++ src/playerobj.js
@@ -18,7 +18,7 @@ var PlayerObj = function(game, x, y)
 	this.walkStepSoundCounter = 0;
 	
 	this.uid = game.getLocalStorageInt(STORAGE_PLAYER_UID, 0);
-	this.name = game.getLocalStorageString(STORAGE_PLAYER_NAME);
+	this.name = game.getLocalStorageString(STORAGE_PLAYER_NAME, '');
 	this.colors = [
 		game.getLocalStorageArray(STORAGE_PLAYER_COLOR_PREFIX + '0', [ 255, 255, 0 ] ),
 		game.getLocalStorageArray(STORAGE_PLAYER_COLOR_PREFIX + '1', [ 0, 128, 192 ] ),
